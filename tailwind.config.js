71ab7eafffac27315cefdeca85e6c3f465210c93
Paulo <8630187+papawlo@users.noreply.github.com>
2020-04-30 19:47:04 +0100

    Part 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,58 @@
 module.exports = {
   theme: {
     extend: {},
+    customForms: theme => ({      
+      default: {
+        'input, textarea, multiselect, select': {
+          borderRadius: theme('borderRadius.lg'),
+          backgroundColor: theme('colors.gray.700'),
+          borderColor:'transparent'
+        },
+        'radio, checkbox':{
+          backgroundColor: theme('colors.gray.900'),
+          borderColor:'transparent',
+          width: '1.5em',
+          height: '1.5em',
+          '&:checked': {
+            backgroundColor: theme('colors.gray.500'),
+          },
+          '&:focus': {
+            backgroundColor: theme('colors.gray.700'),
+          }
+        },
+        // 'input, textarea, multiselect': {
+        //   backgroundColor: theme('colors.gray.900'),
+        // },
+        input: {
+          // borderRadius: theme('borderRadius.lg'),
+          backgroundColor: theme('colors.gray.200'),
+          '&:focus': {
+            backgroundColor: theme('colors.white'),
+          }
+        },
+        select: {          
+          // borderRadius: theme('borderRadius.lg'),
+          // boxShadow: theme('boxShadow.default'),
+          color: theme('colors.white'),
+          lineHeight: theme('lineHeight.snug'),
+          '&:focus': {            
+            boxShadow: theme('boxShadow.outline.none'),
+            borderColor: 'transparent',
+            backgroundColor: theme('colors.gray.400'),
+            color: theme('colors.gray.900'),
+            fontWeight: theme('fontWeight.semibold')
+          },
+        },
+        // checkbox: {
+        //   '&:checked': {
+        //     backgroundColor: theme('colors.indigo.500'),
+        //   }
+        // },
+      },
+    })
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/custom-forms')
+  ],
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-    //...
-    devServer: {
-      hot: true
-    }
-  };
+// module.exports = {
+//     //...
+//     devServer: {
+//       hot: true
+//     }
+//   };
